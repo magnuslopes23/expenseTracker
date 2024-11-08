b6cb560b5f348c84a2427ae5d443567a9d921e2f
magnuslopes23 <lopesmagnus2324@gmail.com>
2024-11-08 17:08:57 +0000

    implementing basic structure for routes

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,19 @@
 const express = require('express');
 const {connectDB} = require('./db/connectDB');
+const {readdirSync} = require('fs');
+const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT;
 
+//middleware to use json and cors.
+app.use(express.json());
+app.use(cors());
 
+//routes
+readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)));
 connectDB();
-
-app.get('/', (req, res)=>{
-    res.send('<h1>Hello add expense<h1>')
-})
-
-app.get('/add-expense', (req,res) =>{
-    res.send('Hey expense added');
-});
-
 app.listen(port, () =>{
     console.log(`Server is running on port http://localhost:${port}`);
 })
