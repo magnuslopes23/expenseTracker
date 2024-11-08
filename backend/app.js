97f976437e9c44946eb921def852b4ddb71c199b
magnuslopes23 <lopesmagnus2324@gmail.com>
2024-11-08 15:58:29 +0000

    db connection using mongoose and dotenv

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,21 @@
 const express = require('express');
+require('dotenv').config();
+const mongoose = require('mongoose');
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
+
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.DATABASE_URL}`);
+        console.log(`\n MongoDB connected!!! DB HOST: ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log('MONGO connection error:', error);
+        process.exit(1);
+    }
+};
+
+connectDB();
 
 app.get('/', (req, res)=>{
     res.send('<h1>Hello add expense<h1>')
