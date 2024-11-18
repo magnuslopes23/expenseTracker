ffebb92d0ff3e3f675e6db8403435a26665becde
magnuslopes23 <lopesmagnus2324@gmail.com>
2024-11-18 12:46:51 +0000

    create similar structure for expense as income

--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -1,4 +1,5 @@
-const { addIncome, getIncomes, deleteIncomes } = require('../controllers/income');
+const { addIncome, getIncomes, deleteIncome,  } = require('../controllers/income');
+const { addExpense, getExpenses, deleteExpense } = require('../controllers/expense');
 
 const router = require('express').Router();
 
@@ -6,6 +7,11 @@ router.get('/', (req, res)=>{
     res.send('Hello World');
 })
 
-router.post('/add-income', addIncome).get('/get-incomes', getIncomes).delete('/delete-income/:id', deleteIncomes)
+router.post('/add-income', addIncome)
+    .get('/get-incomes', getIncomes)
+    .delete('/delete-income/:id', deleteIncome)
+    .post('/add-expense', addExpense)
+    .get('/get-expenses', getExpenses)
+    .delete('/delete-expense/:id', deleteExpense)
 
 module.exports = router;
