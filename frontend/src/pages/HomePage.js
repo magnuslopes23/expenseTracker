2aa201a577f72efb2f196fc36564cb1b1095aa63
magnuslopes23 <lopesmagnus2324@gmail.com>
2024-11-30 16:02:14 +0000

    home page layout

---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Chart from '../components/Chart';
+import Summary from '../components/Summary';
+import RecentHistory from '../components/RecentHistory';
+import '../styles/HomePage.css'; // Ensure styling is applied
+
+
+const HomePage = () => {
+  return (
+    <div className="dashboard">
+      <h2>All Transactions</h2>
+      <div className="dashboard-grid">
+        <div className="chart-container">
+          <Chart />
+        </div>
+        <div>
+          <RecentHistory /> {/* Removed unnecessary container */}
+        </div>
+      </div>
+      <Summary />
+    </div>
+  );
+};
+
+export default HomePage;
